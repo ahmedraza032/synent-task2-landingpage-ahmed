7b61b12eb947f4b48e6cced83385302bd1a5faf7
ahmedraza032 <ahmedrazasomr512@gmail.com>
2026-05-22 12:07:06 +0500

    feat: polish signup section and header

--- src/sections/Email.jsx
+++ src/sections/Email.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const Email = () => {
   return (
-    <div className="sm:px-[150px] p-8 bg-black flex flex-col sm:flex-row w-full text-white justify-between">
+    <div id="newsletter" className="sm:px-[150px] p-8 bg-black flex flex-col sm:flex-row w-full text-white justify-between">
       <div>
         <h4 className="text-3xl font-bold my-2">
           Want tips & tricks to optimize your flow?
@@ -14,17 +12,17 @@ const Email = () => {
       <div>
         <div className="flex gap-3 items-center">
           <input
-            type="text"
+            type="email"
             placeholder="Email"
             className="w-8/12 py-2 px-2 my-2 rounded-md"
           />
-          <button className="bg-green-400 px-3 py-2 rounded-md">
+          <button type="button" className="bg-green-400 px-3 py-2 rounded-md">
             Notify Me
           </button>
         </div>
         <div>
           We care about the privacy of your data. Read about our{" "}
-          <a href="" className="text-green-300">
+          <a href="#" className="text-green-300">
             privacy policies
           </a>
         </div>
